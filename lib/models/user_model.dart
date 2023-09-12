498c65e8c00497c21b3527dae69409ccca57c9e9
MonicaItzel <a292842@alumnos.uaslp.mx>
2023-09-12 15:36:11 -0600

    Avance modificacion del archivo user_model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,12 +3,24 @@ class User{
   final String name;
   final String middlename;
   final String? surname;
+  final DateTime? birthdate;
+  final String phone;
+  final String club;
+  final bool? activeCaregiver;
+  final String? nameCaregiver;
+  final String? phoneCaregiver;
 
   User({
     this.id,
     required this.name,
     required this.middlename,
-    this.surname
+    this.surname,
+    this.birthdate,
+    required this.phone,
+    required this.club,
+    this.activeCaregiver,
+    this.nameCaregiver,
+    this.phoneCaregiver
   });
 
   Map<String, dynamic> toMap() {
@@ -16,7 +28,13 @@ class User{
       'id': id,
       'name': name,
       'middlename': middlename,
-      'surname': surname
+      'surname': surname,
+      'phone': phone,
+      'birthdate': birthdate,
+      'club': club,
+      'active_caregiver': activeCaregiver,
+      'name_caregiver': nameCaregiver,
+      'caregiver_phone': phoneCaregiver
     };
   }
 }
