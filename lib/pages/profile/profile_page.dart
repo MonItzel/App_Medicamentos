00d035538d6f7f6468e2316d7e4604311262ae87
lortega14 <lortega700@gmail.com>
2023-09-25 10:30:41 -0600

    Create profile_page.dart
    profile page

---
+++ lib/pages/profile/profile_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProfilePage();
+  }
+}
+
+class _ProfilePage extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
