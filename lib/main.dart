c3b14f70d8b5e3bb9cd3a46be2b417a913a02421
lortega14 <lortega700@gmail.com>
2023-09-28 10:43:52 -0600

    nueva dependencia de navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:app_medicamentos/pages/start_page.dart';
 import 'package:app_medicamentos/pages/home_page.dart';
+import 'package:app_medicamentos/pages/profile/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: HomePage(),
+      home: ProfilePage(),
     );
   }
 }
--- lib/pages/profile/profile_page.dart
+++ lib/pages/profile/profile_page.dart
@@ -1,4 +1,6 @@
+import 'package:app_medicamentos/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:app_medicamentos/pages/profile/profile_page.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -13,8 +15,82 @@ class _ProfilePage extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color(0xFFEDF2FA),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: AppBar(
+          title: Text(
+            'Perfil',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          leading: IconButton(
+            icon: const Icon(
+                Icons.arrow_back_rounded, color: Color(0xFF09184D)),
+            onPressed: () {
+              Navigator.pushAndRemoveUntil <dynamic>(
+                context,
+                MaterialPageRoute <dynamic>(
+                    builder: (BuildContext context) => HomePage()
+                ),
+                    (route) => false,
+              );
+            },
+          ),
+          actions: const [],
+          backgroundColor: const Color(0xFFEDF2FA),
+          automaticallyImplyLeading: false,
+          centerTitle: false,
+          elevation: 0,
+        ),
+      ),
 
+      body: Padding(
+        padding: EdgeInsets.all(30.0), // Define el padding deseado
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Nombre',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            Text(
+              'Fecha de nacimiento',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            Text(
+              'Dirección',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            Text(
+              'Cuidador',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
-
 }
