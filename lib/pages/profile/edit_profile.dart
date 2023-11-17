388e3693b7e0229d6eca2185457cb2e8977ed149
Luis Eduardo Carreón Soto <lecarreonfmv@gmail.com>
2023-11-17 07:45:46 -0600

    Edit profile
    Edicion del perfil lista (Esta actualizando la informacion del cuidador)

--- lib/pages/profile/edit_profile.dart
+++ lib/pages/profile/edit_profile.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:app_medicamentos/pages/home_page.dart';
 import 'package:app_medicamentos/pages/profile/profile_page.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class EditProfile extends StatefulWidget {
   const EditProfile({super.key});
@@ -51,6 +53,7 @@ class _EditProfile extends State <EditProfile> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           TextFormField(
+            controller: nombreUController,
             obscureText: false,
             textAlign: TextAlign.left,
             decoration: InputDecoration(
@@ -69,6 +72,7 @@ class _EditProfile extends State <EditProfile> {
           ),
           SizedBox(height: 20.0,),
           TextFormField(
+            controller: apellidoUController,
             obscureText: false,
             textAlign: TextAlign.left,
             decoration: InputDecoration(
@@ -88,6 +92,7 @@ class _EditProfile extends State <EditProfile> {
           ),
           SizedBox(height: 20.0,),
           TextFormField(
+            controller: telefonoUController,
             obscureText: false,
             textAlign: TextAlign.left,
             decoration: InputDecoration(
@@ -105,8 +110,72 @@ class _EditProfile extends State <EditProfile> {
               hintText: 'Teléfono',
             ),
           ),
+
+          SizedBox(height: 20.0,),
+
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
+            child: Container(
+              width: 193,
+              height: 77,
+              child: ElevatedButton(
+                onPressed: () {
+                  UpdateProfile();
+                  Navigator.pushAndRemoveUntil <dynamic>(
+                    context,
+                    MaterialPageRoute <dynamic>(
+                        builder: (BuildContext context) => HomePage()
+                    ),
+                        (route) => false,
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF0063C9),
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    )
+                ),
+                child: Text("Siguiente",
+                  style: TextStyle(
+                      fontSize: 26
+                  ),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
   }
+
+  void UpdateProfile() async {
+    Database database = await openDatabase(
+        join(await getDatabasesPath(), 'medicamentos.db'), version: 1);
+
+    await database.transaction((txn) async {
+
+      var usuario = {
+        'cuidador_nombre': nombreUController.text + " " + apellidoUController.text,
+        'cuidador_telefono': telefonoUController.text
+      };
+
+      var id1 = txn.update('Usuario', usuario);
+
+      print("Usuario actualizado: " + usuario.toString());
+    });
+
+    nombreController.text = "";
+    apellidoPController.text = "";
+    apellidoMController.text = "";
+    fechaNacController.text = "";
+    calleController.text = "";
+    coloniaController.text = "";
+    numExteriorController.text = "";
+    cuidadorController.text = "";
+  }
+
+  TextEditingController nombreUController = TextEditingController();
+  TextEditingController apellidoUController = TextEditingController();
+  TextEditingController telefonoUController = TextEditingController();
 }
